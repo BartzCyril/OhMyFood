61c430dd070413161b5fac85991427a4d625fdfd
Cyril BARTZ <bartzcyril@gmail.com>
2023-03-02 17:46:03 +0100

    suppression du js

--- spinner/spinner.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', function() {
-    const loadingSpinner = document.querySelector('.loading-spinner');
-    setTimeout(function() {
-      loadingSpinner.remove()
-    }, 3000);
-  });
